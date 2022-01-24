d22a6e55c952752d3635506188ffb53b4556905d
Seille, Hoel (Mineral Resources, Kensington WA) <Hoel.Seille@csiro.au>
2022-01-24 12:43:57 +0800

    The transdMT executable has been updated.
    The option to save the posterior likelihood and number of layers for all the iterations is added. This allows for monitoring the inversion convergeance.
    The shell scripts have been modified to include this option. The results are saved to a .csv file.
    The runMCMC.sh script and the text file defining the inversion parameters have been added to the scripts folder.

--- scripts/runMCMC.sh
+++ scripts/runMCMC.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-params=$1
+project=$1
+params='inversionParameters.txt'
 
-#read transdMT parameters file
 
 
 echo "\n 
@@ -10,19 +10,33 @@ echo "\n
 ##   Trans-dimensional MCMC 1D Inversion  ##
 ##        of Magnetotelluric Data         ##
 ##                                        ##
-##       Gerhard Visser / Hoël Seillé     ##
+##       Hoël Seillé / Gerhard Visser     ##
 ##            CSIRO DEI FSP 2020          ##
 ############################################"
 
-
+#read transdMT inversion parameters file
 echo ' '
 echo '\nInversion parameters:'
 while IFS='=' read -r param val
 do
+   i=$((i+1))
+   eval "P$i"=$val
    echo "   $param = $val"
 done < "$params"
 echo ' '
 
+
+i=0
+while IFS='=' read -r param val
+do
+   i=$((i+1))
+   eval "P$i"=$val
+done < "$params"
+
+
+
+
+cd ../projects/$project/transdMT
 
 input_dir=../csv
 output_dir=outfolder
@@ -32,7 +46,7 @@ output_dir=outfolder
 for file in $input_dir/*.csv; do #loop1
 	siteid=$(basename "$file" .csv)
 	mkdir -p ${output_dir}/${siteid}
-	bash multithreads.sh $params $siteid $input_dir $output_dir
+	bash multithreads.sh $params $siteid $input_dir $output_dir $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8 $P9 $P10
 done #end loop1
 
 echo "goodbye!
